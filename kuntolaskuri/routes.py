06627039188a78716852b5589d675c170df77a8b
theskelinen <ssahlman1@gmail.com>
2023-06-13 09:38:58 +0300

    login page route and template

--- kuntolaskuri/routes.py
+++ kuntolaskuri/routes.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 routes = Blueprint("routes", __name__)
 
 @routes.route("/")
 def index():
     return render_template("home.html")
+
+@routes.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
